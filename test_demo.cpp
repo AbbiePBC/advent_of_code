db27f90c4dc6adbe3ab16024f52e1f19865a10a7
José Miguel Mut López <jose.mut@intrasonics.com>
2021-12-07 16:06:33 +0000

    test_demo.cpp: fix typo

--- test_demo.cpp
+++ test_demo.cpp
@@ -4,11 +4,11 @@
 
 TEST(TestDemo, hello_test) {
     std::cout << "Hello test" << std::endl;
-    ASSERT_EQUALS(2, 3) << "How come 2 is not the same as 3??";
+    ASSERT_EQ(2, 3) << "How come 2 is not the same as 3??";
 }
 TEST(Dec1_parse, process_input) {
 // whatever
     //process_input("2,3 -> 4,5", lhs, rhs);
-    //ASSERT_EQUALS(lhs.x, 2);
+    //ASSERT_EQ(lhs.x, 2);
     // ...
 }
